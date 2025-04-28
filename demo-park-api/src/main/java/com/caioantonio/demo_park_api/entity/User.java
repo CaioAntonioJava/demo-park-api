bda7b8b310eb25e9c40ee4497717caee4dec6edd
CaioAntonioJava <caioantonio.dev@gmail.com>
2025-04-28 18:22:42 -0300

    recurso de usuario criado

---
+++ demo-park-api/src/main/java/com/caioantonio/demo_park_api/entity/User.java
@@ -0,0 +1,73 @@
+package com.caioantonio.demo_park_api.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true, length = 100)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 200)
+    private String password;
+
+    @Column(name = "role", nullable = false, length = 25)
+    @Enumerated(EnumType.STRING) // transforma o nome da CONST em uma STRING p/ salvar no banco.
+    private Role role;
+
+    public enum Role {
+        ROLE_ADMIN, ROLE_CLIENT
+    }
+
+//    Auditoria:
+
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+
+    @Column(name = "modification_date")
+    private LocalDate modificationDate;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "modified_by")
+    private String modifiedBy;
+
+//    EqualsAndHashCode
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                '}';
+    }
+}
