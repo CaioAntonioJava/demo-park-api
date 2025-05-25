7192b0d74db35ad1a19e4a10c939b0f3a98da869
CaioAntonioJava <caioantonio.dev@gmail.com>
2025-05-25 16:15:18 -0300

    implementado o sistema de autenticação

--- demo-park-api/src/main/java/com/caioantonio/demo_park_api/entity/User.java
+++ demo-park-api/src/main/java/com/caioantonio/demo_park_api/entity/User.java
@@ -4,6 +4,11 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 import java.time.LocalDate;
@@ -15,6 +20,7 @@ import java.util.Objects;
 @NoArgsConstructor
 @Entity
 @Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
 public class User implements Serializable {
 
     @Id
@@ -38,15 +44,19 @@ public class User implements Serializable {
 
 //    Auditoria:
 
+    @CreatedDate
     @Column(name = "creation_date")
     private LocalDateTime creationDate;
 
+    @LastModifiedDate
     @Column(name = "modification_date")
     private LocalDate modificationDate;
 
+    @CreatedBy
     @Column(name = "created_by")
     private String createdBy;
 
+    @LastModifiedBy
     @Column(name = "modified_by")
     private String modifiedBy;
 
