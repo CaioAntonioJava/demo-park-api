454fd74af5cbab0d43d469a157640c584eeba73b
CaioAntonioJava <caioantonio.dev@gmail.com>
2025-05-25 09:02:49 -0300

    criada classe de controller p/ autenticação

---
+++ demo-park-api/src/main/java/com/caioantonio/demo_park_api/web/dto/UserLoginDto.java
@@ -0,0 +1,23 @@
+package com.caioantonio.demo_park_api.web.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class UserLoginDto {
+    @NotBlank
+    @Email(message = "Formato do e-mail inválido", regexp = "^[a-z0-9.+-]+@[a-z0-9.-]+\\.[a-z]{2,}$")
+    private String username;
+
+    @NotBlank
+    @Size(min = 6, max = 6)
+    private String password;
+}
