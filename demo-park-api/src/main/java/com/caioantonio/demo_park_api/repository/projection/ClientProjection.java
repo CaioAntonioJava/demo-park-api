3ef9e0515b304a23cce06df2ae95ad14f0dd918d
CaioAntonioJava <caioantonio.dev@gmail.com>
2025-06-17 11:39:32 -0300

    documentação p/ recursos de criar, recuperar cliente pelo id e listagem de todos os clientes

--- demo-park-api/src/main/java/com/caioantonio/demo_park_api/repository/projection/ClientProjection.java
+++ demo-park-api/src/main/java/com/caioantonio/demo_park_api/repository/projection/ClientProjection.java
@@ -2,7 +2,6 @@ package com.caioantonio.demo_park_api.repository.projection;
 
 
 public interface ClientProjection {
-
     Long getId();
     String getName();
     String getCpf();
--- demo-park-api/src/main/java/com/caioantonio/demo_park_api/web/controller/ClientController.java
+++ demo-park-api/src/main/java/com/caioantonio/demo_park_api/web/controller/ClientController.java
@@ -8,16 +8,29 @@ import com.caioantonio.demo_park_api.service.UserService;
 import com.caioantonio.demo_park_api.web.dto.ClientCreateDto;
 import com.caioantonio.demo_park_api.web.dto.ClientResponseDto;
 import com.caioantonio.demo_park_api.web.dto.PageableDto;
+import com.caioantonio.demo_park_api.web.dto.UserResponseDto;
 import com.caioantonio.demo_park_api.web.dto.mapper.ClientMapper;
 import com.caioantonio.demo_park_api.web.dto.mapper.PageableMapper;
+import com.caioantonio.demo_park_api.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import static io.swagger.v3.oas.annotations.enums.ParameterIn.QUERY;
 
 @RestController
 @RequestMapping("api/v1/clients")
@@ -27,6 +40,20 @@ public class ClientController {
     private final ClientService clientService;
     private final UserService userService;
 
+    @Operation(
+            summary = "Criar um novo cliente", description = "Recurso para criar um novo cliente vinculado a um usuário cadastrado. " +
+            "Requisição exige uso de um bearer token. Acesso restrito a ADMIN",
+            security = @SecurityRequirement(name = "security"),
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ClientResponseDto.class))),
+                    @ApiResponse(responseCode = "409", description = "Cliente CPF já possuí cadastro no sistema",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422", description = "Recurso não processado por dados de entrada inválidos",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "403", description = "Recurso não permitido ao perfil de ADMIN",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @PostMapping
     @PreAuthorize("hasRole('CLIENT')")
     public ResponseEntity<ClientResponseDto> create(
@@ -37,6 +64,17 @@ public class ClientController {
         return ResponseEntity.status(201).body(ClientMapper.toResponseDto(client));
     }
 
+    @Operation(summary = "Localizar um cliente", description = "Recurso para localizar um cliente pelo ID. " +
+            "Requisição exige uso de um bearer token. Acesso restrito a ADMIN",
+            security = @SecurityRequirement(name = "security"),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Recurso localizado com sucesso",
+                            content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = ClientResponseDto.class))),
+                    @ApiResponse(responseCode = "404", description = "Cliente não encontrado",
+                            content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "403", description = "Recurso não permito ao perfil de CLIENTE",
+                            content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @GetMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ClientResponseDto> getById(@PathVariable Long id) {
@@ -44,9 +82,36 @@ public class ClientController {
         return ResponseEntity.ok().body(ClientMapper.toResponseDto(client));
     }
 
+    @Operation(summary = "Recuperar lista de clientes",
+            description = "Requisição exige uso de um bearer token. Acesso restrito a ADMIN ",
+            security = @SecurityRequirement(name = "security"),
+            parameters = {
+                    @Parameter(in = QUERY, name = "page",
+                            content = @Content(schema = @Schema(type = "integer", defaultValue = "0")),
+                            description = "Representa a página retornada"
+                    ),
+                    @Parameter(in = QUERY, name = "size",
+                            content = @Content(schema = @Schema(type = "integer", defaultValue = "5")),
+                            description = "Representa o total de elementos por página"
+                    ),
+                    @Parameter(in = QUERY, name = "sort", hidden = true,
+                            array = @ArraySchema(schema = @Schema(type = "string", defaultValue = "name,asc")),
+                            description = "Representa a ordenação dos resultados. Aceita multiplos critérios de ordenação são suportados.")
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Recurso recuperado com sucesso",
+                            content = @Content(mediaType = " application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ClientResponseDto.class))
+                    ),
+                    @ApiResponse(responseCode = "403", description = "Recurso não permito ao perfil de CLIENTE",
+                            content = @Content(mediaType = " application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class))
+                    )
+            })
     @GetMapping()
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<PageableDto> getAllClients(Pageable pageable) {
+    public ResponseEntity<PageableDto> getAllClients(@Parameter(hidden = true)
+                                                     @PageableDefault(size = 5, sort = {"name"}) Pageable pageable) {
         Page<ClientProjection> clients = clientService.getAllClientes(pageable);
         return ResponseEntity.ok(PageableMapper.toPageDto(clients));
     }
